2f8be06ce5c286b75bebf44099706f2fef0963ba
kindawong <kindawong@aa1bae0d-ba29-0410-9611-f7b7655375bb>
2007-05-12 10:17:29 +0000

    GPS start point with UART

---
+++ trunk/Source/PIC18F4431 Test Programs/SW_GPS/PIC18F4431 GPS.c
@@ -0,0 +1,79 @@
+/*
+	Title: 	PIC18F4431 SPI.c
+
+	Author:	Kyle Wong
+
+	Date:	15/04/07
+	
+	Description:	This program will initiate communications with an external EEPROM via
+					the SPI port.
+
+	Notes:	
+
+	Compiled with MCC18 compiler (v8.01PL3)
+	and MPLAB ver 7.5
+*/
+/************************** Include Section **************************/
+#include <p18f4431.h>
+
+
+/*************************** Program Control *************************/
+
+
+/*************************** Defines *********************************/
+
+
+/*************************** Definitions *****************************/
+typedef union
+{
+	char byte[2];
+	int D_byte;
+	long D_word;
+}WORD;
+
+/*************************** Global Variables ************************/
+WORD Capture;
+
+/*************************** Initialization Functions ****************/
+
+
+/*************************** Function Definitions ********************/
+
+
+/*************************** Interrupt Section ***********************/
+void CCPINT(void);
+#pragma interrupt INTERRUPT
+void INTERRUPT(void)
+{
+	if(PIR1bits.CCP1IF)
+	{
+		CCPINT();
+	}
+}
+
+void CCPINT(void)
+{
+	PIR1bits.CCP1IF = 0; // clear the capture flag
+	Capture.byte[0] = TMR1L;
+	TMR1L = 0;	// Clear the timers
+	Capture.byte[1] = TMR1H;
+	TMR1H = 0;	// Clear the timers
+}
+
+#pragma code high_int_vector = 0x08
+void interrupt_at_high_vector(void)
+{
+	_asm GOTO INTERRUPT _endasm
+}
+#pragma code /* return to default code section */
+
+void main(void)
+{
+
+	do
+	{
+	
+	}
+	while(1);
+}
+
