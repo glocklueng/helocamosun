d32ccb8aa31e51fdcacd9ddf09694627fdab5e4a
kindawong <kindawong@aa1bae0d-ba29-0410-9611-f7b7655375bb>
2007-05-12 10:17:29 +0000

    GPS start point with UART

---
+++ Source/PIC18F4431 Test Programs/SW_GPS/USART.h
@@ -0,0 +1,29 @@
+// USART.h
+
+union USART
+{
+  unsigned char val;
+  struct
+  {
+    unsigned RX_NINE:1;         // Receive Bit 8 if 9-bit mode is enabled
+    unsigned TX_NINE:1;         // Transmit Bit 8 if 9-bit mode is enabled
+    unsigned FRAME_ERROR:1;     // Framing Error for USART
+    unsigned OVERRUN_ERROR:1;   // Overrun Error for USART
+    unsigned fill:4;
+  };
+};
+
+extern union USART USART_Status;
+
+/* Define Section */
+void SerialInit(void);
+void putsUSART (char *data);
+void WriteUSART (char data);
+char ReadUSART (void);
+void getsUSART (char *buffer,unsigned char len);
+
+#define DataRdyUSART( ) (PIR1bits.RCIF)
+#define getcUSART ReadUSART
+#define BusyUSART( ) (!TXSTAbits.TRMT)
+#define putcUSART WriteUSART
+
